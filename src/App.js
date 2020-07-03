d346e2c74c44a5463a588bd64ad667e693505160
Lauren West <laurenwest12@gmail.com>
2020-07-03 01:04:42 -0400

    allows user to add and remove from chat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Brandon from './images/Brandon.jpg';
+import Lauren from './images/Lauren.jpg';
+import Sarah from './images/Sarah.jpg';
+
+const options = [
+  { name: 'Brandon', image: Brandon },
+  { name: 'Lauren', image: Lauren },
+  { name: 'Sarah', image: Sarah },
+];
+
+const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      added: [],
+      options: shuffle(options),
+    };
+  }
+
+  handleAdd = ({ target }) => {
+    const { name } = target;
+    const { image } = this.state.options.find((item) => item.name === name);
+    const added = this.state.added.slice();
+
+    const isAlreadyAdded =
+      added.filter((item) => item.name === name).length === 1;
+
+    !isAlreadyAdded && added.push({ name, image });
+
+    const options = this.state.options.filter((item) => item.name !== name);
+
+    this.setState({ added, options });
+  };
+
+  handleRemove = ({ target }) => {
+    const { name } = target;
+    const { image } = this.state.added.find((item) => item.name === name);
+
+    const options = this.state.options.slice();
+    options.push({ name, image });
+
+    const added = this.state.added.filter((item) => item.name !== name);
+
+    this.setState({ added, options });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Chat</h1>
+        <div className="added">
+          {this.state.added.map(({ name, image }) => (
+            <div key={name} className="cast">
+              <img src={image} alt={name} className="cast-image" />
+              <div className="cast-text">{name}</div>
+
+              <button
+                className="btn-remove"
+                name={name}
+                onClick={this.handleRemove}
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+        </div>
+        <h1>Others</h1>
+        <div className="added">
+          {this.state.options.map(({ name, image }) => (
+            <div key={name} className="cast">
+              <img src={image} alt={name} className="cast-image" />
+              <div className="cast-text">{name}</div>
+              <button className="btn-add" name={name} onClick={this.handleAdd}>
+                Add
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
